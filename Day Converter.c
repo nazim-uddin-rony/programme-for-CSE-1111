1096eb5fc35b6c7e250c8ec9b0c062a8e7d4d26f
Md Nazimuddin Rony <139897525+nazim-uddin-rony@users.noreply.github.com>
2023-07-25 06:44:11 +0600

    Day Converter.c

--- Day Converter.c
+++ Day Converter.c
@@ -2,14 +2,14 @@
 int main(){
 /* Input the total Days by user */
 int Day, day, week, year;
-printf("Enter Days:\n\n");
+printf("Enter total Days:\n\n");
 scanf("%d", &Day);
 /* Conversion Formula for Day to Year, Week and Days*/
 year = Day / 365;
-week = (Day%365)/7;
-day = (Day%365)%7;
+week = (Day-(year*365))/7;
+day = Day-(week*7)-(year*365);
 
-printf("\n\n%d Years %d Weeks %d Days\n\n", year, week, day);
+printf("\n\n%d Days = %d Years %d Weeks %d Days\n\n",Day, year, week, day);
 
 return 0;
 
